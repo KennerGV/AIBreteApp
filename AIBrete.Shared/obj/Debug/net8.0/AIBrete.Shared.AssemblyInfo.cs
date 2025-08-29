e173ddabf4866d759d1240d67437a95493b1297a
Kener Gutierrez Vasquez <kgutierrez@atesa.local>
2025-08-29 15:01:52 -0600

    Subida archivo .gitignore

--- AIBrete.Shared/obj/Debug/net8.0/AIBrete.Shared.AssemblyInfo.cs
+++ AIBrete.Shared/obj/Debug/net8.0/AIBrete.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIBrete.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c5e4810484beff19ab252b970197837d24c017a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIBrete.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIBrete.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
